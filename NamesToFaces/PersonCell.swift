d6d12abf7cdcaaf7bdadcb87792b30217f463b8a
alibekshak <alibekshakiro1@gmail.com>
2023-12-06 09:51:09 +0600

    App

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,12 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Apple on 06.12.2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
---
+++ NamesToFaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Apple on 06.12.2023.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
